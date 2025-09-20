362f55b4d532d06c1d8831824d42e7d5a4b07c15
mariya-koz <mkozyreva868@gmail.com>
2025-09-20 12:18:28 +0300

    Создала список и заполнила его

--- yjkk/list/list/Program.cs
+++ yjkk/list/list/Program.cs
@@ -10,6 +10,15 @@ namespace list
     {
         static void Main(string[] args)
         {
+            List<string> list = new List<string>();
+            Console.WriteLine("Исходный размер списка = " + list.Count);
+            list.Add("Маша");
+            list.Add("Ирина");
+            list.Add("Кира");
+            list.Add("Егор");
+            list.Add("Алина");
+            Console.WriteLine("Размер списка после добавления элемента " + list.Count);
+
         }
     }
 }
