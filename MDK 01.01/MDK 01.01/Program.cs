26ffa7e61916c04ab348f88138c1fd69a279c366
Kseniapav <kseniapavlova5916@gmail.com>
2025-09-25 09:09:43 +0300

    Создала массив

--- MDK 01.01/MDK 01.01/Program.cs
+++ MDK 01.01/MDK 01.01/Program.cs
@@ -10,7 +10,16 @@ namespace MDK_01._01
     {
         static void Main(string[] args)
         {
-
+            // С клавиатуры ввести массив из n вещественных чисел.
+            Console.WriteLine("Сколько элементов будет в массиве: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            Console.WriteLine("Введите элементы массива: ");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+           
         }
     }
 }
