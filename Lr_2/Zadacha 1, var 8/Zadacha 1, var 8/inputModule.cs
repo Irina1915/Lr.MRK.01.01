c77be57fd6b0a9094b9e779c239273ca397df089
Daniil5903 <st50@WS50-07>
2025-10-11 13:04:34 +0300

    Создала класс но не доделала

---
+++ Lr_2/Zadacha 1, var 8/Zadacha 1, var 8/inputModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Zadacha_1__var_8
+{
+    public class inputModule
+    {
+        public (List<string>[], List<int>[] ) InputApplicantsByCenre()
+        {
+            List<string>[] applicantsBySpecialties = new List<string>[3];
+            List<int>[] counts = new List<int>[3];
+
+            //Заполняем абитуриентов по специальности программирование (НУЛЕВОЙ ИНДЕКС)
+            applicantsBySpecialties[0].Add("Берёзкин Егор");
+            counts[0].Add(4);
+
+            applicantsBySpecialties[0].Add("Берёзкин Егор");
+            counts[0].Add(4);
+
+            applicantsBySpecialties[0].Add("Берёзкин Егор");
+            counts[0].Add(4);
+
+            applicantsBySpecialties[0].Add("Берёзкин Егор");
+            counts[0].Add(4);
+
+            applicantsBySpecialties[0].Add("Берёзкин Егор");
+            counts[0].Add(4);
+
+            return (applicantsBySpecialties, counts);
+        }
+    }
+}
