a009f4e6375b787c5ae5b9777e12e5ce3c9cc88f
Daniil5903 <st50@WS50-07>
2025-10-11 11:42:23 +0300

    Создала проект

---
+++ Lr_2/Zadacha 1, var 8/Zadacha 1, var 8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_1__var_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
