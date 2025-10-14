6c154040b17c4946d218e18b244f42721fd60b9b
mariya-koz <mkozyreva868@gmail.com>
2025-10-14 09:16:41 +0300

    Создала массив

---
+++ LR_1/№1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // С клавиатуры ввести массив из n вещественных чисел.
+            Console.WriteLine("Сколько элементов будет в массиве: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            Console.WriteLine("Введите элементы массива: ");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
