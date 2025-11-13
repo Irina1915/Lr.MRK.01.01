b1d1818134e771affbee93d8358bd5aa827ae239
st403 <st403@WS403-06>
2025-11-13 11:33:45 +0300

    Создала класс Telephone_sales_analysis

--- Distant/Distant/Sale_of_phones.cs
+++ Distant/Distant/Sale_of_phones.cs
@@ -8,5 +8,16 @@ namespace Distant
 {
     internal class Sale_of_phones
     {
+        public DateTime Date { get; set; }     // Дата продажи (метка времени)
+        public string PhoneModel { get; set; } // Модель телефона
+        public int Quantity { get; set; }      // Количество проданных единиц
+        public double Price { get; set; }      // Цена за единицу для клиента
+        public double Cost { get; set; }       // Затраты магазина на телефоны
+
+        // Вычисление выгоды магазина
+        public double Profit => (Price - Cost) * Quantity;
+
+        // Вычисление дохода магазина
+        public double Revenue => Price * Quantity;
     }
 }
