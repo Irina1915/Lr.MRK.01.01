75350607fa33327c9b096cfde80d653f153bd1d0
st403 <st403@WS403-06>
2025-11-13 11:32:12 +0300

    Создала класс Sale_of_phones

---
+++ Distant/Distant/Sale_of_phones.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distant
+{
+    internal class Sale_of_phones
+    {
+    }
+}
