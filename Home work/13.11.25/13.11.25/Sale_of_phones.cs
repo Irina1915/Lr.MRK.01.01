7224ab1b5d15cc57fac89247f72af10727a7199d
st403 <st403@WS403-06>
2025-11-17 10:57:26 +0300

    Исправила задачку

---
+++ Home work/13.11.25/13.11.25/Sale_of_phones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._11._25
+{
+    public struct Sale_of_phones
+    {
+        public DateTime Date;     // Дата продажи (метка времени)
+        public string PhoneModel; // Модель телефона
+        public int Quantity;      // Количество проданных единиц
+        public double Price;      // Цена за единицу для клиента
+
+        // Вычисление дохода магазина
+        public double Revenue => Price * Quantity;
+    }
+}
