323e0aa085458eb6080f382ccb8c50eff22e7ae5
mariya-koz <mkozyreva868@gmail.com>
2026-02-17 09:37:49 +0300

    доделать

---
+++ ModelView/ModelViewMyForms/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using ModelViewBiblioteka.Model;
+using ModelViewBiblioteka.Model_Views;
+using ModelViewBiblioteka.Presenter;
+using ModelViewBiblioteka.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModelViewMyForms
+{
+    public partial class MainForm : Form
+    {
+        private UserPresenter presenter_;
+        public MainForm()
+        {
+            InitializeComponent();
+            UsersTableView usertable = new UsersTableView();
+            Controls.Add(usertable);
+            usertable.Dock = DockStyle.Top;
+
+            UserPresenter userPresenter = new UserPresenter(new MemoryUserModel(), usertable);
+        }
+
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Вы точно хотите удалить пользователя?", "Внимание!",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                List<User> selectetUsers = UserView.GetSelectedUsers();
+                presenter_.Remove(selectetUsers);
+            }
+        }
+    }
+}
