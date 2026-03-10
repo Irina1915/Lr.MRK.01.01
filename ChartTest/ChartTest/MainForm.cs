4161fa2805748008d32165505902fe29e1b0cdb5
mariya-koz <mkozyreva868@gmail.com>
2026-03-10 11:19:21 +0300

    Сделана диаграмма

---
+++ ChartTest/ChartTest/MainForm.cs
@@ -0,0 +1,82 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using SalesLibrary;
+using SalesLibrary.Analysis;
+using SalesLibrary.Presenters;
+using SalesLibrary.Views;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace ChartTest
+{
+    public partial class MainForm : Form
+    {
+        private SalesPresenter presenter_;
+        void FillCartesianChart()
+        {
+            ItemsList.DataSource = presenter_.GetAllItems();
+            ItemsList.DisplayMember = "Name";
+            if (ItemsList.Items.Count > 0)
+            {
+                presenter_.ShowSalesByItem(((Item)ItemsList.Items[0]).Name);
+            }
+        }
+
+        void FillAngular()
+        {
+            angular.FromValue = 0;
+            angular.ToValue = 100;
+
+            angular.TicksForeground = Brushes.Red;
+            angular.NeedleFill = Brushes.DarkRed;
+        }
+
+        void FillSolid()
+        {
+            solid.From = 0;
+            solid.To = 100;
+            solid.LabelFormatter = value => value + "%";
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+
+            presenter_ = new SalesPresenter(new List<ISalesView> { cartesian });
+            PieChart.SetPresenter(presenter_);
+            PieChart.UpdateView();
+
+            FillCartesianChart();
+
+            FillAngular();
+
+            FillSolid();
+
+        }
+
+        private void ItemsList_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            Item selectedItem = ((Item)(ItemsList.SelectedItem));
+            if(selectedItem == null)
+            {
+                return;
+            }
+
+            presenter_.ShowSalesByItem(selectedItem.Name);
+            double percent = Math.Round(
+                presenter_.GetProfitPercentByItem(selectedItem), 2);
+
+            angular.Value = percent;
+            solid.Value = percent;
+        }
+
+        void FillPieChart()
+        {
+
+
+            PieChart.LegendLocation = LegendLocation.Right;
+        }
+
+    }
+}
---
+++ ChartTest/ChartTest/Views/SalesPieChart.cs
@@ -0,0 +1,58 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using SalesLibrary;
+using SalesLibrary.Presenters;
+using SalesLibrary.Views;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+using System.Windows.Media;
+
+namespace ChartTest.Views
+{
+    class SalesPieChart : LiveCharts.WinForms.PieChart
+                                       
+    {
+        private LineSeries seriesData_;
+        public SalesPieChart()
+        {
+            seriesData_ = new LineSeries
+            {
+                Title = "Продажи",
+
+                Stroke = new SolidColorBrush(Colors.Blue),
+                StrokeThickness = 2,
+
+                PointGeometry = DefaultGeometries.Circle,
+                PointGeometrySize = 7,
+
+            };
+        }
+        private SalesPresenter presenter_;
+
+        public void SetPresenter (SalesPresenter presenter)
+        {
+            presenter_ = presenter;
+        }
+
+        public void UpdateView()
+        {
+            List<Item> allItems = presenter_.GetAllItems();
+            SeriesCollection seriesData = new SeriesCollection();
+            foreach(Item item in allItems)
+            {
+                seriesData.Add(new PieSeries
+                {
+                    Title = item.Name,
+                    Values = new ChartValues<double>() { presenter_.GetProfitPercentByItem(item)}
+                });
+
+            }
+            Series = seriesData;
+        }
+    }
+}
