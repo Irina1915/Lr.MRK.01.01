c638a25ad09007d5d8b8985b53392ae86d11463e
Fa1tloy <baranovdaniil534@gmail.com>
2026-03-13 10:23:05 +0300

    Доделать

---
+++ Lr_grafic/Lr_grafic/Views/SalesCartesianChart.cs
@@ -0,0 +1,16 @@
+﻿using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Lr_grafic.Views
+{
+    internal class SalesCartesianChart
+    {
+        
+
+    }
+}
---
+++ Lr_grafic/Lr_grafic/Views/SalesPieChart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LiveCharts;
+using LiveCharts.WinForms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lr_grafic.Views
+{
+    internal class SalesPieChart
+    {
+    }
+}
